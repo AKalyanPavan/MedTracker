2096bca34e73b2befdf74616acf9f074ff1615af
AKalyanPavan <pavanaravelli690@gmail.com>
2024-01-23 22:30:21 -0500

    Adds starting files while creating a new Xcode project

---
+++ MedTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedTracker
+//
+//  Created by Ravi on 2024-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
