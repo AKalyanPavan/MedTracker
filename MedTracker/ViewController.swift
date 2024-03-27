8a2ce753632005fbcfcf11c4476a53b13f9e2607
Tahmid Hassan <tahmid.hassan@gmail.com>
2024-03-26 22:57:17 -0400

    Issue 11: Users can be reminded to take their medicine at a time of the day

--- MedTracker/ViewController.swift
+++ MedTracker/ViewController.swift
@@ -7,15 +7,26 @@
 
 import UIKit
 import SwiftUI
+import UserNotifications
 
 class ViewController: UIViewController {
     
     var medTrackers: [MedTracker] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                print("Notification permission granted")
+            } else {
+                print("Notification permission denied")
+            }
+        }
+        
+        scheduleNotification()
     }
-
+    
     @IBAction func gotoCreateTracker(_ sender: Any) {
         let createNewStoryBoard = UIStoryboard(name: "Main", bundle: nil)
         let createNewViewController = createNewStoryBoard.instantiateViewController(withIdentifier: "CreateNewTrackerViewController") as! CreateNewTrackerViewController
@@ -24,10 +35,10 @@ class ViewController: UIViewController {
         self.navigationController!.pushViewController(createNewViewController, animated: true)
     }
     @IBAction func gotoMyTrackers(_ sender: Any) {
-//        let myTrackersStoryBoard = UIStoryboard(name: "Main", bundle: nil)
-//        let myTrackersViewController = myTrackersStoryBoard.instantiateViewController(withIdentifier: "MyTrackersViewController") as! MyTrackersViewController
-//        myTrackersViewController.navigationItem.hidesBackButton = true
-//        self.navigationController!.pushViewController(myTrackersViewController, animated: true)
+        //        let myTrackersStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        //        let myTrackersViewController = myTrackersStoryBoard.instantiateViewController(withIdentifier: "MyTrackersViewController") as! MyTrackersViewController
+        //        myTrackersViewController.navigationItem.hidesBackButton = true
+        //        self.navigationController!.pushViewController(myTrackersViewController, animated: true)
         
         let trackersListView = TrackersListView(viewModel: TrackersListViewModel())
         let hostingController = UIHostingController(rootView: trackersListView)
@@ -39,6 +50,24 @@ class ViewController: UIViewController {
         physiciansViewController.navigationItem.hidesBackButton = true
         self.navigationController!.pushViewController(physiciansViewController, animated: true)
     }
-    
+   
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder"
+        content.body = "Don't forget to take your medicine!"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: "MedicineReminder", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled successfully")
+            }
+        }
+    }
 }
 
